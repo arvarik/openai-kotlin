f65d61a4597964b19b38a25b8d9dbcc6a0feece8
Arvind Arikatla <arvind.arikatla@gmail.com>
2022-12-27 19:14:29 -0500

    Fixing unit test typo

--- openai-kotlin-core/src/test/kotlin/org/arvarik/openai/core/api/moderations/CreateModerationResponseTest.kt
+++ openai-kotlin-core/src/test/kotlin/org/arvarik/openai/core/api/moderations/CreateModerationResponseTest.kt
@@ -38,6 +38,6 @@ val createModerationResponse = CreateModerationResponse(
 class CreateModerationResponseTest : DataClassSerializationCommonTest<CreateModerationResponse>(
     serializer(),
     createModerationResponse,
-    expectedSerializedString = """{"id":"Test id","model":"Test model","results":[{"categories":{"hate":false,"hate/threatening":false,"self-harm":false,"sexual":false,"sexual/minors":false,"violence":true,"violence/graphics":true},"category_scores":{"hate":0.1,"hateAndThreatening":0.2,"selfHarm":0.3,"sexual":0.4,"sexualAndMinors":0.5,"violence":0.7,"violenceAndGraphic":0.6},"flagged":true}]}""",
+    expectedSerializedString = """{"id":"Test id","model":"Test model","results":[{"categories":{"hate":false,"hate/threatening":false,"self-harm":false,"sexual":false,"sexual/minors":false,"violence":true,"violence/graphic":true},"category_scores":{"hate":0.1,"hate/threatening":0.2,"self-harm":0.3,"sexual":0.4,"sexual/minors":0.5,"violence":0.7,"violence/graphic":0.6},"flagged":true}]}""",
     invalidSerializedString = """{"model":"Test model","results":[{"categories":{"hate":false,"hate/threatening":false,"self-harm":false,"sexual":false,"sexual/minors":false,"violence":true,"violence/graphics":true},"category_scores":{"hate":0.1,"hateAndThreatening":0.2,"selfHarm":0.3,"sexual":0.4,"sexualAndMinors":0.5,"violence":0.7,"violenceAndGraphic":0.6},"flagged":true}]}"""
 )
