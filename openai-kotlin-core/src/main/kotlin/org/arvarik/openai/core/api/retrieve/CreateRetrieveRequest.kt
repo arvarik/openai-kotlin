2fcb285aac947a7a6b35bc9d4f1c0a331a1813d4
Amol Bhagavathi <abhagavathi21@gmail.com>
2022-12-30 20:05:01 -0800

    Created the retrieve request and response class

---
+++ openai-kotlin-core/src/main/kotlin/org/arvarik/openai/core/api/retrieve/CreateRetrieveRequest.kt
@@ -0,0 +1,18 @@
+package org.arvarik.openai.core.api.retrieve
+
+import kotlinx.serialization.Serializable
+import org.arvarik.openai.core.api.OpenAIRequest
+
+
+/**
+ * Documentation: https://beta.openai.com/docs/api-reference/models/retrieve
+ */
+
+@Serializable
+data class CreateRetrieveRequest(
+    /**
+     * ID of the model to use for request
+     */
+    val model: String
+
+) : OpenAIRequest
