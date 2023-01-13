be462ce5860e9b0d807d63c0f19af9648078646c
Amol Bhagavathi <abhagavathi21@gmail.com>
2023-01-12 16:54:35 -0800

    Deleted unneeded import statements

--- openai-kotlin-core/src/jvmTest/kotlin/org/arvarik/openai/core/api/models/RetrieveRequestTest.kt
+++ openai-kotlin-core/src/jvmTest/kotlin/org/arvarik/openai/core/api/models/RetrieveRequestTest.kt
@@ -2,7 +2,6 @@ package org.arvarik.openai.core.api.models
 
 import kotlinx.serialization.serializer
 import org.arvarik.openai.core.api.common.DataClassSerializationCommonTest
-import org.arvarik.openai.core.api.models.RetrieveModelRequest
 
 val retrieveModelRequest = RetrieveModelRequest(
     model = "text-ada-001"
